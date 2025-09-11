b4057dd23ed494fa0c5a06ecb5b969d1b5be622d
matheus.santos <matheus.santos@grupo3778.com.br>
2025-09-11 00:15:14 -0300

    feat: adding likelihoods naive-bayes

--- methods/naive_bayes.py
+++ methods/naive_bayes.py
@@ -1,8 +1,6 @@
-from sklearn.preprocessing import OrdinalEncoder as OE
 import numpy as np
 import pandas as pd
 
-encoder = OE()
 
 def train_bayes(data, target, features):
 
@@ -10,8 +8,21 @@ def train_bayes(data, target, features):
     totais_y = data[target].value_counts()
     prior =  totais_y / len(data[target])
 
-    
+    total_y = data[target].value_counts().to_dict()
 
     print(y_classes)
     print(totais_y)
     print(prior)
+
+    likelihoods = []
+    for col in features:
+        likelihood = data[[target, col]].value_counts().reset_index(name='count')
+        
+        likelihood['prob'] = likelihood.apply(
+            lambda row: row['count'] / total_y[row[target]], axis=1
+        )
+    
+        likelihoods.append(likelihood)
+
+    print(likelihoods)
+    
