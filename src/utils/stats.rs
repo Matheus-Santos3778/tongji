1a34755ab24e99d4063d83a24df37f98775fedae
matheus.santos <matheus.santos@grupo3778.com.br>
2025-11-17 20:01:50 -0300

    feat: adding fn for matricial operations

--- src/utils/stats.rs
+++ src/utils/stats.rs
@@ -13,3 +13,30 @@ pub fn mean(x: [i64; 5]) -> Option<f64>{
     mean
 
 }
+
+pub fn vector_minus_scalar(x: [i64; 5], scalar: i64) -> [i64; 5] {
+
+    let result: [i64; 5] = x.map(|val| val - scalar);
+
+    result
+
+}
+
+pub fn dot_product(x: [i64; 5], y: [i64; 5]) -> i64 {
+
+    let product: i64 = x.iter()
+                        .zip(y.iter())
+                        .map(|(a, b)| a * b)
+                        .sum();
+
+    product
+
+}
+
+pub fn round_number(num: f64, decimal_places: u32) -> f64 {
+
+    let factor = 10f64.powi(decimal_places as i32);
+
+    (num * factor).round() / factor
+
+}
