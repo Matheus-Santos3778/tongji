1523ec7276ba03d674a5dbc4f7d50eacb362f031
matheus.santos <matheus.santos@grupo3778.com.br>
2025-11-17 20:03:28 -0300

    feat: finishing linear regressions calculations

--- src/reg/lin.rs
+++ src/reg/lin.rs
@@ -1,8 +1,26 @@
-use crate::utils::stats;
+use crate::utils::stats::{self, round_number};
 
-pub fn fit_model(arr_x: [i64; 5], _arr_y: [f64; 5]){
+pub fn fit_model(arr_x: [i64; 5], arr_y: [i64; 5]) -> (f64, f64) {
 
     let x_mean: Option<f64> = stats::mean(arr_x);
 
-    println!("Media de x: {:?}", x_mean);
+    let y_mean: Option<f64> = stats::mean(arr_y);
+
+    let x_by_mean: [i64; 5] = stats::vector_minus_scalar(arr_x, x_mean.unwrap() as i64);
+
+    let y_by_mean: [i64; 5] = stats::vector_minus_scalar(arr_y, y_mean.unwrap() as i64);
+
+    let dot_prod_xy: i64 = stats::dot_product(x_by_mean, y_by_mean);
+
+    let dot_prod_xx: i64 = stats::dot_product(x_by_mean, x_by_mean);
+
+    let slope: f64 = dot_prod_xy as f64 / dot_prod_xx as f64;
+    
+    let intercept: f64 = y_mean.unwrap() - slope * x_mean.unwrap();
+
+    println!("Coeficiente angular (slope): {:?}", slope);
+
+    println!("Coeficiente linear (intercept): {:?}", intercept);
+
+    (round_number(slope, 2), round_number(intercept, 2))
 }
